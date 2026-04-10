0767171d8f44dc3f6b9cdca0350e1612b399cb59
TOM-PRIME-X <saofficeal44@gmail.com>
2026-04-10 21:14:07 +0600

    Update clearsession.js

--- commands/clearsession.js
+++ commands/clearsession.js
@@ -8,8 +8,8 @@ const channelInfo = {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '',
+            newsletterName: '𝐓𝐎𝐌 𝐏𝐑𝐈𝐌𝐄 𝐗',
             serverMessageId: -1
         }
     }
